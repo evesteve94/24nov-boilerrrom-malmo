7ad93547bbacc6e6bc27d21cd59aa95e4e768aa4
Eva Björling <eva.bjorling@chasacademy.se>
2023-11-24 11:32:23 +0100

    finished our functions for single portion recipes, moving on to portion control ala viktväktarna

--- script/getrecipe.js
+++ script/getrecipe.js
@@ -1,3 +1,4 @@
+
 //hämta info från json
 async function getRecipe(string){
     //hämta vår array
@@ -5,25 +6,79 @@ async function getRecipe(string){
     
     //if-sats för response
     if(response.ok){
+        let ingString = '';
         const recipes = await response.json();
         console.log(recipes);
-        if(string = 'kladdkaka'){
-            //hämtar vi ingridienser
-            // let name = recipes[0].recipeName
-            //long-ass template string förslag:
-            textRecipe.textContent = ``
-        } 
-        if (string = 'banankaka'){
+        recipeContainer.innerHTML = '';
+        if(string == 'kladdkaka'){
+            //loopa ingredients
+            recipes.kladdkaka.ingredients.forEach(function(item){
+                ingString +=` <p>${item.baseAmount} ${item.unit} ${item.name} </p>`;
+            });
 
+            //fyller vår container
+            recipeContainer.innerHTML = 
+            `<div class ="recipe">
+            <h3>${recipes.kladdkaka.recipeName}</h3>
+            <p><strong>Portioner: ${recipes.kladdkaka.portions}</strong></p> Ingredienser:
+            ${ingString}
+            </div>`; 
         }
-        if (string = 'tiramisu'){
-            
+        if (string == 'banankaka'){
+            //loopa ingredients
+            recipes.banankaka.ingredients.forEach(function(item){
+                ingString +=` <p>${item.baseAmount} ${item.unit} ${item.name} </p>`;
+            });
+
+            //fyller vår container
+            recipeContainer.innerHTML = 
+            `<div class ="recipe">
+            <h3>${recipes.banankaka.recipeName}</h3>
+            <p><strong>Portioner: ${recipes.banankaka.portions}</strong></p> Ingredienser:
+            ${ingString}
+            </div>`;             
         }
-        if (string = 'kanelbullar'){
-            
+        if (string == 'tiramisu'){
+            //loopa ingredients
+            recipes.tiramisu.ingredients.forEach(function(item){
+                ingString +=` <p>${item.baseAmount} ${item.unit} ${item.name} </p>`;
+            });
+
+            //fyller vår container
+            recipeContainer.innerHTML = 
+            `<div class ="recipe">
+            <h3>${recipes.tiramisu.recipeName}</h3>
+            <p><strong>Portioner: ${recipes.tiramisu.portions}</strong></p> Ingredienser:
+            ${ingString}
+            </div>`;  
         }
-        if (string = 'lemon'){
-            
+        if (string == 'kanelbullar'){
+            //loopa ingredients
+            recipes.kanelbullar.ingredients.forEach(function(item){
+                ingString +=` <p>${item.baseAmount} ${item.unit} ${item.name} </p>`;
+            });
+
+            //fyller vår container
+            recipeContainer.innerHTML = 
+            `<div class ="recipe">
+            <h3>${recipes.kanelbullar.recipeName}</h3>
+            <p><strong>Portioner: ${recipes.kanelbullar.portions}</strong></p> Ingredienser:
+            ${ingString}
+            </div>`;            
+        }
+        if (string == 'lemon'){
+            //loopa ingredients
+            recipes.lemon.ingredients.forEach(function(item){
+                ingString +=` <p>${item.baseAmount} ${item.unit} ${item.name} </p>`;
+            });
+
+            //fyller vår container
+            recipeContainer.innerHTML = 
+            `<div class ="recipe">
+            <h3>${recipes.lemon.recipeName}</h3>
+            <p><strong>Portioner: ${recipes.lemon.portions}</strong></p> Ingredienser:
+            ${ingString}
+            </div>`;              
         }
     } else{
         console.log(`HTTP error message: ${response.status}`)
