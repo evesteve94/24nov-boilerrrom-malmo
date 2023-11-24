644ccc199f443d867f9ef1d7081550f1ca5d5bb6
Eva Björling <eva.bjorling@chasacademy.se>
2023-11-24 09:18:41 +0100

    our first commit, index and main css/js are added with the main structure incl

---
+++ script/main.js
@@ -0,0 +1,22 @@
+
+const recipes = [
+    {
+    recipeName: "Chokladkaka",
+    portions: 1,
+    ingredients: [
+        { name: "Mjöl", baseAmount: 1, unit: "kopp" },
+        { name: "Socker", baseAmount: 0.5, unit: "kopp" },
+        { name: "Kakao", baseAmount: 2, unit: "msk" }
+    ],
+    instructions: "Blanda alla ingredienser och grädda i 175 grader i 20 minuter."
+},
+{
+    recipeName: "Vaniljkakor",
+    portions: 1,
+    ingredients: [
+        // Ingrediensdata för Vaniljkakor...
+    ],
+    instructions: "..."
+}
+// Lägg till fler receptobjekt här
+];
