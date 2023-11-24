b9613f4fef428ba922b287225494ccda949c911e
Eva Björling <eva.bjorling@chasacademy.se>
2023-11-24 09:57:44 +0100

    js updated with links to all our html elements, reday for some functions baby

--- script/main.js
+++ script/main.js
@@ -1,22 +1,11 @@
+//kopplar till alla våra html element
+const kladdkakaBtn = document.getElementById('chokladkaka-btn');
+const bananBtn = document.getElementById('banankaka-btn');
+const tiramisuBtn = document.getElementById('tiramisu-btn');
+const kanelbullarBtn = document.getElementById('kanelbullar-btn');
+const lemonBtn = document.getElementById('lemon-btn');
 
-const recipes = [
-    {
-    recipeName: "Chokladkaka",
-    portions: 1,
-    ingredients: [
-        { name: "Mjöl", baseAmount: 1, unit: "kopp" },
-        { name: "Socker", baseAmount: 0.5, unit: "kopp" },
-        { name: "Kakao", baseAmount: 2, unit: "msk" }
-    ],
-    instructions: "Blanda alla ingredienser och grädda i 175 grader i 20 minuter."
-},
-{
-    recipeName: "Vaniljkakor",
-    portions: 1,
-    ingredients: [
-        // Ingrediensdata för Vaniljkakor...
-    ],
-    instructions: "..."
-}
-// Lägg till fler receptobjekt här
-];
+const portionCount = document.getElementById('portion-count');
+const updatePortionsBtn = document.getElementById('update-portions');
+
+const recipeContainer = document.getElementById('recipe-container');
